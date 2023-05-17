9ded45323f9e019c3dceaed3e2bcf60c4c01cfa4
Rafaela Rodrigues <79214722+RafsRodrigues@users.noreply.github.com>
2023-05-17 20:54:17 -0300

    alteracoes

--- info.php
+++ info.php
@@ -79,7 +79,7 @@ $rs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         <div class="infos_pet_clock">
           <span><i class="uil uil-clock"></i> Publicado em:</span>
-          <span style="color:black;font-size:15px"><?php echo date('d/m/Y', strtotime( $obj['dtLog'])); ?></span>
+          <span style="color:black;font-size:15px"><?php echo inverteData($obj['dtLog']); ?></span>
         </div>
   
       </div>
@@ -101,4 +101,15 @@ $rs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <?php } ?>
 
-<?php require_once('footer.php'); ?>
+<?php require_once('footer.php'); 
+
+function inverteData($data){
+  if(count(explode("/",$data)) > 1){
+      return implode("-",array_reverse(explode("/",$data)));
+  }elseif(count(explode("-",$data)) > 1){
+      return implode("/",array_reverse(explode("-",$data)));
+  }
+}
+
+
+?>
