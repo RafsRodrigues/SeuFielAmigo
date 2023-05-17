dcf2c5f8c57021c1dbd1bdf75e2915277362e294
Rafaela Rodrigues <79214722+RafsRodrigues@users.noreply.github.com>
2023-05-17 19:38:25 -0300

    alteracao

--- connection/conexao.php
+++ connection/conexao.php
@@ -36,7 +36,7 @@ class ConnectionFactory{
       $username = "bac941f85fad3e";
       $password = "486678ad";
 
-      $pdo = new PDO('mysql:host='.$servername.';dbname='.$database.'', $username, $password);
+      $pdo = new PDO('mysql:host='.$servername.';dbname='.$database.'', $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e){
       print "Error!:" . $e->getMessage() . "<br/>";
