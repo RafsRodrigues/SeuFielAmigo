4ca435eab29f6f2fb39954905ffcf0812a314101
Rafaela Rodrigues <79214722+RafsRodrigues@users.noreply.github.com>
2023-05-17 20:48:19 -0300

    alteracoes

--- info.php
+++ info.php
@@ -79,7 +79,7 @@ $rs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         <div class="infos_pet_clock">
           <span><i class="uil uil-clock"></i> Publicado em:</span>
-          <span style="color:black;font-size:15px"><?php echo implode('/', array_reverse(explode('-', $obj['dtLog']))); ?></span>
+          <span style="color:black;font-size:15px"><?php echo DateTime::createFromFormat('d/m/Y', $obj['dtLog']); ?></span>
         </div>
   
       </div>
